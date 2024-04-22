cb153999cc0d7b66b96842fdace344dc4a1ae512
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-04-22 11:51:19 +0200

    update styles nav

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+export const NavContainer = styled.ul`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+@media (min-width: 1440px) {
+display: flex;
+flex-direction: row;
+gap: 30px;
+}
+`
+
 export const NavLinkStyled = styled(NavLink)`
   text-decoration: none;
 color:red;
   font-weight: 500;
-  font-size: 14px;`;
+  font-size: 14px;
+  text-align: center;
+  `;
 
-  export const NavContainer = styled.ul`
-    display: flex;
-  `
+
