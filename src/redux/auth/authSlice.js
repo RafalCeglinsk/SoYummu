6e97eeb480a66d28ad10c785d1864f4526262a41
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-04-22 09:29:45 +0200

    Redux auth, categories, favorites

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { register, login, logout, refreshUser } from "./operations";
+
+const persistedToken = localStorage.getItem("auth:token");
+
+const isPendingAction = (action) => {
+  return action.type.endsWith("/pending");
+};
+
+const isRejectAction = (action) => {
+  return action.type.endsWith("/rejected");
+};
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: persistedToken || null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addMatcher(isPendingAction, handlePending)
+      .addMatcher(isRejectAction, handleRejected)
+      .addDefaultCase((state) => {
+        state.error = "someone use old function, fix it!";
+      });
+  },
+});
+
+export const authReducer = authSlice.reducer;
