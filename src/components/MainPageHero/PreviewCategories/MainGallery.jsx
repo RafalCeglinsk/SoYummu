d8ab19b9c456f81878d27b00dfb2c2939adc6177
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-04-24 17:15:53 +0200

    copy previous work - main page component & page

---
+++ src/components/MainPageHero/PreviewCategories/MainGallery.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { GalleryUl, MainGalleryH2, ButtonWrapper } from "./MainGallery.styled";
+import { PreviewCategories } from "./PreviewCategories";
+// import { fetchCategories } from "../../../api/mainRecipesApi";
+// import { getViewMode } from "../../../api/viewModeUtils";
+import { ButtonSeeAll } from "../../Buttons/GalleryButtonSeeAll";
+
+export const MainGallery = () => {
+  const [categories, setCategories] = useState([]);
+  const [viewMode, setViewMode] = useState(getViewMode());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewMode(getViewMode());
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <GalleryUl>
+      {categories.map((categoryRecipes, _id) => (
+        <div key={_id}>
+          <MainGalleryH2>{categoryRecipes[0].category}</MainGalleryH2>
+          <PreviewCategories
+            categoryRecipes={categoryRecipes}
+            viewMode={viewMode}
+          />
+          <ButtonWrapper>
+            <Link to={`/categories/${categoryRecipes[0].category}`}>
+              <ButtonSeeAll text="See All" />{" "}
+            </Link>
+          </ButtonWrapper>
+        </div>
+      ))}
+    </GalleryUl>
+  );
+};
