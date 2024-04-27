aea6d8348206742bd5ad5c941c64b0b83c6fd2f7
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-04-27 21:00:16 +0200

    prettier

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -2,46 +2,46 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const NavContainer = styled.ul`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-gap:40px;
-padding-top:15vw;
-
-@media (min-width: 1440px) {
-display: flex;
-flex-direction: row;
-gap:40px;
-padding-top:0px;
-}
-&.NavFooter{
+  display: flex;
   flex-direction: column;
-  padding-top:0px;
-  gap:24px;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  padding-top: 15vw;
 
-}
-&.NavFooter>a{
-  color:var(  --color-text-7);
-  font-size:14px;
-}
-`
-
+  @media (min-width: 1440px) {
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
+    padding-top: 0px;
+  }
+  &.NavFooter {
+    flex-direction: column;
+    padding-top: 0px;
+    gap: 14px;
+    @media (min-width: 1440px) {
+      gap: 24px;
+    }
+  }
+  &.NavFooter > a {
+    color: var(--color-text-7);
+    font-size: 14px;
+    letter-spacing: -2%;
+  }
+`;
 
 export const NavLinkStyled = styled(NavLink)`
   text-decoration: none;
   font-weight: 500;
-  font-size:18px;
+  font-size: 18px;
   letter-spacing: -0.36px;
 
-  color:var(--color-text-6);
+  color: var(--color-text-6);
   transition: color var(--transition-time) var(--cubic);
 
-
-  &:hover,&:focus,
+  &:hover,
+  &:focus,
   &.active {
-    color: var(  --color-text-5);
+    color: var(--color-text-5);
   }
-  `;
-
-
+`;
