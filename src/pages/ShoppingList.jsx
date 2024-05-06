8b9fe74f861407f43a67c9e65ddff873b12c1459
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-05-06 11:25:52 +0200

    add PageTittle component and footer

--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -1,8 +1,11 @@
-
+import { PageTitle } from "../components/PageTitle/PageTitle";
+import Footer from "../components/Footer/Footer";
 function ShoppingList() {
     return (
       <div >
-  <p>THIS IS ShoppingList PAGE </p>
+        <PageTitle>Shopping List </PageTitle>
+  <p>****************************THIS IS ShoppingList PAGE </p>
+  <Footer/>
       </div>
     );
   }
