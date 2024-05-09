a5e1f5aebccae8c1ccef78039a5c9f7e87a64d25
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-05-09 19:49:52 +0200

    FavoritesElement
    Created one Element to display both - MyRecipes and Favorites components

--- src/components/Favorites/FavoriteRecipies/FavoriteRecipies.jsx
+++ src/components/Favorites/FavoriteRecipies/FavoriteRecipies.jsx
@@ -1,53 +1,20 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getFavorites } from '../../../redux/Favorites/operations';
-import { selectFavorites } from '../../../redux/recipePage/selectors';
-
-import NotFoundRecipe from '../NotFoundRecipe/NotFoundRecipe';
-import FavoriteCard from '../FavoriteCard/FavoriteCard';
-
-import {
-  Container,
-  FavoriteContainer,
-  FavoriteList,
-  FavoriteItem,
-} from './FavoriteRecipies.styled';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectToken } from "../../../redux/auth/selectors";
+import { FavoritesElement } from "../../FavoritesElement/FavoritesElement";
+import { selectFavorites } from "../../../redux/recipes/favorites/selectors";
+import { getFavorites } from "../../../redux/recipes/favorites/operations";
 
 const FavoriteRecipes = () => {
   const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.auth);
-  const  favorites  = useSelector(selectFavorites)
-  console.log(token)
-  console.log(favorites) 
+  const token = useSelector(selectToken);
+  const favorites = useSelector(selectFavorites);
 
   useEffect(() => {
     dispatch(getFavorites(token));
-    console.log(getFavorites)
   }, [token, dispatch]);
 
-
- 
-  return (
-    <Container>
-      <FavoriteContainer>
-
-        <FavoriteList>
-          {favorites.length !== 0 ? (
-            favorites.map(recipe => (
-              <FavoriteItem key={recipe._id}>
-                <FavoriteCard
-                  recipe={recipe}
-
-                />
-              </FavoriteItem>
-            ))
-          ) : (
-            <NotFoundRecipe message={"You don't have favorite recipes yet..."} />
-          )}
-        </FavoriteList>
-      </FavoriteContainer>
-    </Container>
-  );
+  return <FavoritesElement recipes={favorites} />;
 };
 
 export default FavoriteRecipes;
--- src/components/MyRecipies/MyRecipe.jsx
+++ src/components/MyRecipies/MyRecipe.jsx
@@ -1,63 +1,18 @@
-import { Link } from "react-router-dom";
-import {
-  FavRecipeItem,
-  RecipeImageThumb,
-  RecipeImage,
-  RecipeTitle,
-  RecipeDescription,
-  RecipePrepTime,
-  DeleteButton,
-  SeeRecipeBtn,
-  FavRecipeTopInfo,
-} from "./MyRecipe.styled.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { selectToken } from "../../redux/auth/selectors.js";
 import { selectOwnRecipe } from "../../redux/recipes/recipes/selectors.js";
-import {
-  deleteRecipe,
-  getMyRecipes,
-} from "../../redux/recipes/recipes/operations.js";
+import { getMyRecipes } from "../../redux/recipes/recipes/operations.js";
+import { FavoritesElement } from "../FavoritesElement/FavoritesElement.jsx";
 
 export const MyRecipe = () => {
   const token = useSelector(selectToken);
   const recipes = useSelector(selectOwnRecipe);
-  console.log(recipes);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getMyRecipes(token));
   }, [dispatch, token]);
 
-  const onDelete = (id) => {
-    dispatch(deleteRecipe(id, token));
-  };
-
-  if (!recipes) {
-    return null;
-  }
-  return (
-    <>
-      {recipes.map((recipe) => (
-        <FavRecipeItem>
-          <Link to={`/recipe/${recipe._id}`}>
-            <RecipeImageThumb>
-              <RecipeImage src={recipe.thumb} alt={recipe.title} />
-            </RecipeImageThumb>
-          </Link>
-          <FavRecipeTopInfo>
-            <div>
-              <RecipeTitle>{recipe.title}</RecipeTitle>
-              <RecipeDescription>{recipe.description}</RecipeDescription>
-            </div>
-            <RecipePrepTime>{recipe.time}</RecipePrepTime>
-          </FavRecipeTopInfo>
-          <DeleteButton onClick={() => onDelete(recipe._id)}></DeleteButton>
-          <Link to={`/recipe/${recipe._id}`}>
-            <SeeRecipeBtn>See Recipe</SeeRecipeBtn>
-          </Link>
-        </FavRecipeItem>
-      ))}
-    </>
-  );
+  return <FavoritesElement recipes={recipes} />;
 };
