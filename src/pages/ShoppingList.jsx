ecaf0e83e81bdfc0e98cb07e542d3be062e51d4a
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-05-19 11:59:42 +0200

    Render Footer on ShommpingList Page

--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -1,11 +1,13 @@
 import { PageTitle } from "../components/PageTitle/PageTitle.jsx";
 import IngredientsShoppingList from "../components/IndegredientsShoppingList/IndegredientsShoppingList.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 
 const ShoppingList = () => {
   return (
     <>
       <PageTitle>Shopping List</PageTitle>
       <IngredientsShoppingList />
+      <Footer/>
     </>
   );
 };
